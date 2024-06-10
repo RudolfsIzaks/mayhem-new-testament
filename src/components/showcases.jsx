dcec03990cc10dc4d4f9261fd9b40fc9dce3c582
Rudolfs Izaks <rudolfsizaks@gmail.com>
2024-06-10 19:31:53 +0300

    Mobile Ops

--- src/components/showcases.jsx
+++ src/components/showcases.jsx
@@ -25,53 +25,53 @@ function Testimonials() {
 
   return (
     <>
-      <h1 className="text-4xl text-white italic font-montserrat font-black text-center mt-10 mb-10">
+      <h1 className="text-3xl md:text-4xl text-white italic font-montserrat font-black text-center mt-10 mb-10">
         YOU NAME IT, WE CONQUER.
       </h1>
-      <div className="flex flex-col gap-5 px-20 ">
-        <div className="custom_card rounded-md w-full flex gap-10 p-10">
+      <div className="flex flex-col gap-5 px-4 md:px-20">
+        <div className="custom_card rounded-md w-full flex flex-col md:flex-row gap-5 md:gap-10 p-5 md:p-10">
           <div>
             <div>
-              <img src={praksis} width={300} alt="Praksis Logo" />
-              <h2 className="italic font-black font-montserrat text-3xl grad-headline px-5 pt-5">Project: Praksis</h2>
-              <p className="text-white font-montserrat font-semibold px-5 pb-5">AKA - PROJECT LEAD FLOOD... </p>
+              <img src={praksis} className="w-48 md:w-60" alt="Praksis Logo" />
+              <h2 className="italic font-black font-montserrat text-xl md:text-3xl grad-headline md:px-5 pt-5">Project: Praksis</h2>
+              <p className="text-white font-montserrat font-semibold md:px-5 pb-5">AKA - PROJECT LEAD FLOOD... </p>
             </div>
-            <p className="px-5 text-white font-montserrat">Generated 100's signups for Praksis (Biggest legal software in the Balkans) with hyper-targeted FB ads which lead to a "dollar-store" price per lead of 0.88 EUR (the average is between 5-7 eur)!</p>
-            <button className="bg-yellow border-2 border-yellow px-5 my-5 mx-5 h-12 rounded-sm font-black text-xl hover:bg-transparent hover:text-yellow hover:scale-110 duration-200" onClick={handleNavigation}>Start Your SaaS Success</button>
+            <p className="md:px-5 text-white font-montserrat">Generated 100's signups for Praksis (Biggest legal software in the Balkans) with hyper-targeted FB ads which lead to a "dollar-store" price per lead of 0.88 EUR (the average is between 5-7 eur)!</p>
+            <button className="bg-yellow border-2 border-yellow px-5 my-5 md:mx-5 h-10 md:h-12 rounded-sm font-black text-lg md:text-xl hover:bg-transparent hover:text-yellow hover:scale-110 duration-200" onClick={handleNavigation}>Start Your SaaS Success</button>
           </div>
           <div>
             <img src={praksisS} alt="Praksis Showcase" className="rounded-md"/>
-            <p className="text-end text-white italic font-montserrat text-sm">Powered By Mayhem</p>
+            <p className="text-end text-white italic font-montserrat text-xs md:text-sm">Powered By Mayhem</p>
           </div>
         </div>
-        <div className="custom_card rounded-md w-full flex gap-10 p-10 h-auto">
-          <div className="w-1/3">
-            <img src={infernusS} alt="" className=" rounded-md"/>
-            <p className="text-start text-white italic font-montserrat text-sm">Powered By Mayhem</p>
+        <div className="custom_card rounded-md w-full flex flex-col-reverse md:flex-row gap-5 md:gap-10 p-5 md:p-10 h-auto">
+          <div className="w-full md:w-1/3">
+            <img src={infernusS} alt="" className="rounded-md"/>
+            <p className="text-start text-white italic font-montserrat text-xs md:text-sm">Powered By Mayhem</p>
           </div>
-          <div className="w-2/3">
-            <div className="flex flex-col items-end">
-              <img src={infernus} alt="infernus logo" />
-              <h2 className="italic font-black font-montserrat text-3xl grad-headline px-5 pt-5 text-end">Project: Infernus</h2>
-              <p className="text-white font-montserrat font-semibold px-5 pb-5 text-end">AKA - PROJECT ZERO to ONE...</p>
+          <div className="w-full md:w-2/3">
+            <div className="flex flex-col items-start">
+              <img src={infernus} alt="infernus logo" className=" w-24 md:w-32"/>
+              <h2 className="italic font-black font-montserrat text-xl md:text-3xl grad-headline md:px-5 pt-5 text-start">Project: Infernus</h2>
+              <p className="text-white font-montserrat font-semibold md:px-5 pb-5 text-start">AKA - PROJECT ZERO to ONE...</p>
             </div>
-            <p className="px-5 text-white font-montserrat text-end">By crafting powerful video ads and optimizing Infernus' website we pushed the brand out to hundreds of thousands of consumers going from a 0% website conversion rate, catapulting all the way past average, and to a 13.44% iniciate checkout rate.</p>
-            <button className="bg-yellow border-2 border-yellow px-5 my-5 mx-5 h-12 rounded-sm font-black float-end text-xl hover:bg-transparent hover:text-yellow hover:scale-110 duration-200" onClick={handleNavigation}>Start Your E-Com Success</button>
+            <p className="md:px-5 text-white font-montserrat text-start text-sm md:text-base">By crafting powerful video ads and optimizing Infernus' website we pushed the brand out to hundreds of thousands of consumers going from a 0% website conversion rate, catapulting all the way past average, and to a 13.44% initiate checkout rate.</p>
+            <button className="bg-yellow border-2 border-yellow px-5 my-5 md:mx-5 h-10 md:h-12 rounded-sm font-black text-lg md:text-xl hover:bg-transparent hover:text-yellow hover:scale-110 duration-200" onClick={handleNavigation}>Start Your E-Com Success</button>
           </div>
         </div>
-        <div className="custom_card rounded-md w-full flex gap-10 p-10">
-          <div className="w-1/2">
+        <div className="custom_card rounded-md w-full flex flex-col md:flex-row gap-5 md:gap-10 p-5 md:p-10">
+          <div className="w-full md:w-1/2">
             <div>
-              <img src={ACT} alt="" className="w-28 px-5" />
-              <h2 className="italic font-black font-montserrat text-3xl grad-headline pt-5 px-5">Project: ACT!</h2>
-              <p className="text-white font-montserrat font-semibold pb-5 px-5">AKA - PROJECT ZERO to ONE...</p>
+              <img src={ACT} alt="" className="w-20 md:w-28 md:px-5" />
+              <h2 className="italic font-black font-montserrat text-xl md:text-3xl grad-headline pt-5 md:px-5">Project: ACT!</h2>
+              <p className="text-white font-montserrat font-semibold pb-5 md:px-5">AKA - PROJECT ZERO to ONE...</p>
             </div>
-            <p className="px-5 text-white font-montserrat">Act was diverting from their usual online activity and wanted to finally do something different- An in-person event. So, we couldn't say "no". But we only had 3 weeks! Luckily, in just 10 days we started generating tickets priced at 100 EUR each at 5.8X ROAS. Crazy, right?!</p>
-            <button className="bg-yellow border-2 border-yellow px-5 my-5 mx-5 h-12 rounded-sm font-black text-xl hover:bg-transparent hover:text-yellow hover:scale-110 duration-200" onClick={handleNavigation}>Start Your E-Learning Success</button>
+            <p className="md:px-5 text-white font-montserrat text-sm md:text-base">Act was diverting from their usual online activity and wanted to finally do something different- An in-person event. So, we couldn't say "no". But we only had 3 weeks! Luckily, in just 10 days we started generating tickets priced at 100 EUR each at 5.8X ROAS. Crazy, right?!</p>
+            <button className="bg-yellow border-2 border-yellow px-5 my-5 md:mx-5 h-10 md:h-12 rounded-sm font-black text-lg md:text-xl hover:bg-transparent hover:text-yellow hover:scale-110 duration-200" onClick={handleNavigation}>Start Your E-Learning Success</button>
           </div>
-          <div className="w-1/2">
+          <div className="w-full md:w-1/2">
             <img src={ACTs} alt="act showcase" className="rounded-md" />
-            <p className="text-end text-white italic font-montserrat text-sm">Powered By Mayhem</p>
+            <p className="text-end text-white italic font-montserrat text-xs md:text-sm">Powered By Mayhem</p>
           </div>
         </div>
       </div>
--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -35,9 +35,9 @@ function LandingPage() {
       <div className="flex flex-col gap-2 justify-center items-center h-screen px-4 md:px-20 lg:px-40 hero" data-aos="fade-down">
         <div>
           <h3 className="font-montserrat text-lg md:text-2xl mb-5 md:mb-10 text-yellow font-bold italic text-center">
-            ACCELERATE YOUR SAAS BUSINESS & GET:
+            ACCELERATE YOUR ONLINE BUSINESS & GET:
           </h3>
-          <h1 className="text-3xl md:text-5xl font-black italic text-white text-center">
+          <h1 className="text-2xl md:text-5xl font-black italic text-white text-center">
             MORE GROWTH, MORE TURNOVER, MORE CLIENTS, GUARANTEED.
           </h1>
           <p className="font-montserrat text-sm md:text-md mt-5 md:mt-10 text-white text-center">
@@ -56,7 +56,7 @@ function LandingPage() {
         <h1 className="font-montserrat font-black italic text-accent text-3xl md:text-5xl text-center" data-aos="fade-left">
           DON'T BELIEVE US JUST YET?
         </h1>
-        <h2 className="text-white text-xl md:text-2xl font-bold font-montserrat italic " data-aos="fade-left">
+        <h2 className="text-white text-xl md:text-2xl font-bold font-montserrat italic md:text-center " data-aos="fade-left">
           NO WORRIES, HERE IS HOW IT WORKS...
         </h2>
         <iframe
@@ -123,14 +123,14 @@ function LandingPage() {
       </div>
       <div className="mt-20 md:mt-40 flex flex-col gap-3 mx-4 md:mx-20 relative h-auto md:h-screen" data-aos="fade-up">
         <h2 className="grad-headline font-montserrat font-bold italic text-2xl md:text-3xl text-center">The POWER-7 Arsenal</h2>
-        <h1 className="font-montserrat font-black text-3xl md:text-5xl italic text-center text-white px-4 md:px-20">OUR UNIVERSAL ARSENAL FOR 7-FIGURE SaaS DOMINATION</h1>
-        <div className="absolute bg-yellow rounded-lg px-5 py-2 p_1"><p className="font-montserrat font-bold text-lg">Viral Video Ads</p></div>
-        <div className="absolute bg-accent text-white rounded-sm px-4 py-3 p_2"><p className="font-montserrat font-bold text-lg">Funnel CRO</p></div>
-        <div className="absolute bg-yellow rounded-lg px-5 py-2 p_3"><p className="font-montserrat font-bold text-lg">Superior Websites</p></div>
-        <div className="absolute bg-accent text-white px-5 py-2 p_4"><p className="font-montserrat font-bold text-lg">Killer CopyWriting</p></div>
-        <div className="absolute bg-yellow rounded-lg px-5 py-2 p_5"><p className="font-montserrat font-bold text-lg">Strategic Marketing</p></div>
-        <div className="absolute bg-yellow rounded-md px-5 py-2 p_6"><p className="font-montserrat font-bold text-lg">Automated Sales Systems</p></div>
-        <div className="absolute bg-accent text-white rounded-lg px-5 py-2 p_7"><p className="font-montserrat font-bold text-lg">LTV Optimization</p></div>
+        <h1 className="font-montserrat font-black text-2xl md:text-5xl italic text-center text-white px-4 py-5 md:py-0 md:px-20">OUR UNIVERSAL ARSENAL FOR 7-FIGURE  DOMINATION</h1>
+        <div className="lg:absolute bg-yellow rounded-lg px-5 py-2 p_1"><p className="font-montserrat font-bold text-lg">Viral Video Ads</p></div>
+        <div className="lg:absolute bg-accent text-white rounded-sm px-4 py-3 p_2"><p className="font-montserrat font-bold text-lg">Funnel CRO</p></div>
+        <div className="lg:absolute bg-yellow rounded-lg px-5 py-2 p_3"><p className="font-montserrat font-bold text-lg">Superior Websites</p></div>
+        <div className="lg:absolute bg-accent text-white px-5 py-2 p_4"><p className="font-montserrat font-bold text-lg">Killer CopyWriting</p></div>
+        <div className="lg:absolute bg-yellow rounded-lg px-5 py-2 p_5"><p className="font-montserrat font-bold text-lg">Strategic Marketing</p></div>
+        <div className="lg:absolute bg-yellow rounded-md px-5 py-2 p_6"><p className="font-montserrat font-bold text-lg">Automated Sales Systems</p></div>
+        <div className="lg:absolute bg-accent text-white rounded-lg px-5 py-2 p_7"><p className="font-montserrat font-bold text-lg">LTV Optimization</p></div>
       </div>
       <Testimonials/>
     </>
